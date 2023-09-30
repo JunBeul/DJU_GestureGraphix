44caa32adf65bc8bf4a82108dde4a1399d36f80f
JunBeul <yhs06280@naver.com>
2023-09-30 20:40:52 +0900

    마지막 정리

--- frontEnd/web/src/views/messageView.vue
+++ frontEnd/web/src/views/messageView.vue
@@ -7,6 +7,13 @@
 </template>
 
 <style lang="scss" scoped>
+.text{
+  margin-bottom: 25px;
+}
+.back{
+  color: #6faae7;
+  cursor: pointer;
+}
 </style>
 
 <script>
@@ -14,9 +21,14 @@ export default {
   name: "message",
   data() {
     return {
-      title : '',
-      text : ''
+      title : '404에러',
+      text : '해당 페이지는 존재하지 않는 페이지입니다. 주소를 확인해주세요.'
     }
   },
+  methods: {
+    back: function(event){
+      window.history.go(-1);
+    }
+  }
 }
 </script>
